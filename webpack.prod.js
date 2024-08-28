4d152ead00412c727184214e276bcc6880f8f686
NadaAsadk <nadaasad2003@gmail.com>
2024-08-28 19:48:08 +0300

    doonee

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,32 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true
+    },
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/, // Fixed regex pattern for JavaScript files
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // Extract CSS into separate files
+                    'css-loader', // Translates CSS into CommonJS
+                    'sass-loader' // Compiles Sass to CSS
+                ],
             }
         ]
     },
@@ -18,6 +34,15 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
-    ]
-}
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+        runtimeChunk: 'single',
+    }
+};
